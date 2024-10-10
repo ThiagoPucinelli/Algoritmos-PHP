992334b546ee4527395afb17a57a57b968260c31
Thiago Pucinelli <136750405+ThiagoPucinelli@users.noreply.github.com>
2024-10-10 15:04:18 -0300

    Add files via upload

---
+++ listaphp/1.php
@@ -0,0 +1,11 @@
+<?php 
+
+$n = -25;
+
+if ($n >0)
+    echo "<h2>Valor Positivo: $n </h2>";
+elseif ($n <0)
+    echo "<h2>Valor Negativo: $n </h2>";
+else
+    echo "<h2>Valor igual a Zero: $n </h2>";
+?>
